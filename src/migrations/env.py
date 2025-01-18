fc82df88f39c92c5ef8b653a513c1cecc6fe1a5e
twpro8 <twpro8@gmail.com>
2025-01-18 17:45:24 +0300

    add fixture for creating db

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -7,11 +7,8 @@ from alembic import context
 
 from src.config import settings
 from src.database import Base
-from src.models.hotels import HotelsORM
-from src.models.rooms import RoomsORM
-from src.models.users import UsersORM
-from src.models.bookings import BookingsORM
-from src.models.facilities import FacilitiesOrm
+from src.models import * # metadata
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src import settings
+from src.database import Base, null_pool_engine
+from src.models import * # import metadata
+
+
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
+    assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def async_main():
+    async with null_pool_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
