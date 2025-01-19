814c22981202bb53986e4626c2bf4749666b1538
twpro8 <twpro8@gmail.com>
2025-01-19 05:25:37 +0300

    add api facilities tests, post and get

---
+++ tests/integration_tests/facilities/test_api.py
@@ -0,0 +1,23 @@
+from src.schemas.facilities import FacilityAddRequest
+from tests.conftest import read_json
+
+
+async def test_add_facilities(ac):
+    for facility in read_json("mock_facilities"):
+        response = await ac.post(
+            "/facilities",
+            json=facility,
+        )
+        assert response.status_code == 200
+
+async def test_get_facilities(ac):
+    response = await ac.get("/facilities")
+    assert response.status_code == 200
+
+    mock_data = read_json("mock_facilities")
+    response_data = response.json()
+
+    mock_titles = {item['title'] for item in mock_data}
+    response_titles = {item['title'] for item in response_data}
+
+    assert mock_titles == response_titles
