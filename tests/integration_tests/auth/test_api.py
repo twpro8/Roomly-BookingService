380adbb251f4b7d628f9d54d4228a7296d3f547b
twpro8 <twpro8@gmail.com>
2025-01-21 01:05:33 +0300

    add test_auth_users

---
+++ tests/integration_tests/auth/test_api.py
@@ -0,0 +1,82 @@
+import pytest
+
+
+@pytest.mark.parametrize("username, email, password, status_code", [
+    ("Cat Tester", "tester_cat@gmail.com", "love_chicken", 200),
+    ("Car Yugo", "yugish@gmail.com", "not_reliable", 200),
+    ("Pythons", "pypy@gmail.com", "cute_programming", 200),
+    ("Carrot", "pypy@gmail.com", "strong_password", 401),        # has the same email
+    ("Pythons", "watermelon@gmail.com", "tasty_and_juicy", 401), # has the same username
+])
+async def test_auth_users(
+        username,
+        email,
+        password,
+        status_code,
+        ac
+):
+
+    # Register user
+    response = await ac.post(
+        "/users/register",
+        json={
+            "username": username,
+            "email": email,
+            "password": password,
+        }
+    )
+    if status_code == 200:
+        assert response.status_code == status_code
+        assert isinstance(response.json(), dict)
+        assert response.json()["status"] == "ok"
+
+        # Log in user
+        response = await ac.post(
+            "/users/login",
+            json={
+                "username": username,
+                "password": password,
+            }
+        )
+        assert response.status_code == status_code
+        assert isinstance(response.json(), dict)
+        assert response.json()["status"] == "ok"
+        assert response.json()["access_token"] == ac.cookies.get("access_token")
+
+        # Get current user
+        response = await ac.get("/users/profile")
+        assert response.status_code == status_code
+        assert isinstance(response.json(), dict)
+        assert response.json()["username"] == username
+        assert response.json()["email"] == email
+
+        # Log out user
+        response = await ac.post("/users/logout")
+        assert response.status_code == status_code
+        assert isinstance(response.json(), dict)
+        assert response.json()["status"] == "ok"
+        assert not ac.cookies.get("access_token")
+
+        # Get current user
+        response = await ac.get("/users/profile")
+        assert response.status_code == 401
+
+    else:
+        assert response.status_code == status_code
+        assert isinstance(response.json(), dict)
+
+        # Log in user
+        response = await ac.post(
+            "/users/login",
+            json={
+                "username": username,
+                "password": password,
+            }
+        )
+        assert response.status_code == status_code
+        assert isinstance(response.json(), dict)
+        assert not ac.cookies.get("access_token")
+
+        # Get current user
+        response = await ac.get("/users/profile")
+        assert response.status_code == status_code
