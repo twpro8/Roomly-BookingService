931de21212a6f666bb684ceeb13c3fd646456e7b
twpro8 <twpro8@gmail.com>
2025-02-07 01:12:39 +0300

    fix create_room() to add facilities only if exist

--- src/services/rooms.py
+++ src/services/rooms.py
@@ -37,7 +37,7 @@ class RoomService(BaseService):
         rooms_facilities_data = [
             RoomFacilityAdd(room_id=room.id, facility_id=f_id) for f_id in room_data.facilities_ids
         ]
-        if not rooms_facilities_data:
+        if rooms_facilities_data:
             await self.db.rooms_facilities.add_bulk(rooms_facilities_data)
         await self.db.commit()
         return room
