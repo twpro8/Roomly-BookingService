8357d5b26bce6ee2470cf5cd7760e91e8929f06f
twpro8 <twpro8@gmail.com>
2025-02-17 22:08:37 +0300

    add test_get_facilities and test_delete_facility

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -1,17 +1,23 @@
+import pytest
 from typing import Any
 
-import pytest
 from httpx import AsyncClient
+from pydantic import ValidationError
+
+from src.schemas.facilities import Facility
 
 
 @pytest.mark.parametrize(
     "title, status_code, unexpected_field",
     [
-        ("My Facility Title", 200, None),
+        ("Test 1", 200, None),
         ("", 422, None),
+        (None, 422, None),
         ("a" * 100, 200, None),
         ("b" * 101, 422, None),
-        ("My Facility Title 2", 422, "unexpected_field"),
+        ("Test 2", 422, "unexpected_field"),
+        ("Test 3", 200, None),
+        ("Test 4", 200, None),
     ],
 )
 async def test_add_facility(
@@ -33,13 +39,54 @@ async def test_add_facility(
         assert "data" in response_json
 
         facility_id = response_json["data"]["id"]
-        check_exists = await ac.get("/facilities")
-        assert check_exists.status_code == 200
 
-        existing_facilities = check_exists.json()
-        added_facility = next((f for f in existing_facilities if f["id"] == facility_id), None)
+        get_current_facilities = await ac.get("/facilities")
+        assert get_current_facilities.status_code == 200
+
+        current_facilities = get_current_facilities.json()
+        added_facility = next((f for f in current_facilities if f["id"] == facility_id), None)
 
         assert added_facility is not None, f"Facility with id {facility_id} not found"
         assert added_facility["title"] == title, (
             f"Expected title {title}, but got {added_facility['title']}"
         )
+
+
+async def test_get_facilities(ac: AsyncClient) -> None:
+    response = await ac.get("/facilities")
+    assert response.status_code == 200
+    facilities = response.json()
+    assert isinstance(facilities, list)
+
+    for facility in facilities:
+        try:
+            Facility(**facility)
+        except ValidationError as e:
+            assert False, f"Facility data validation failed: {e}"
+
+
+@pytest.mark.parametrize(
+    "f_id, status_code",
+    [
+        (11, 200),
+        (12, 200),
+        (14, 200),
+        (15, 404),
+        (0, 422),
+        (-5, 422),
+        (9999999999, 422),
+        (13, 200),
+    ]
+)
+async def test_delete_facility(ac: AsyncClient, f_id: int, status_code: int) -> None:
+    response = await ac.delete(f"/facilities/{f_id}")
+    assert response.status_code == status_code
+
+    if response.status_code == 200:
+        get_current_facilities = await ac.get("/facilities")
+        assert get_current_facilities.status_code == 200
+
+        current_facilities = get_current_facilities.json()
+        is_deleted = next((f for f in current_facilities if f["id"] == f_id), None)
+
+        assert is_deleted is None, f"Facility with id {f_id} was not deleted or still exists in the list"
